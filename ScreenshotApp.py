a00882b415faed962a6b71e01d0e76263f5a8701
sirvin01 <salimjr201@gmail.com>
2023-03-04 13:39:32 +0300

    Create ScreenshotApp.py

---
+++ ScreenshotApp.py
@@ -0,0 +1,12 @@
+import time
+import pyautogui
+
+def screenshot():
+    name = int(round(time.time() * 1000))
+    name = '{}.png'.format(name)
+    time.sleep(5)
+    img = pyautogui.screenshot(name)
+    img.show()
+
+
+screenshot()
